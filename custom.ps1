2990a18b870564e97ad4a6e19619f28be3190203
admada <73750440+admada@users.noreply.github.com>
2024-04-25 14:57:41 +0200

    Update custom.ps1

--- custom.ps1
+++ custom.ps1
@@ -55,10 +55,51 @@ New-Item -Path 'C:\Temp' -ItemType Directory -Force | Out-Null
 
 ## Install WinGet
 
-Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force
-Set-PSRepository -Name 'PSGallery' -InstallationPolicy Trusted
-Install-Script -Name winget-install -Force
-winget-install.ps1 -Force
+# Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force
+# Set-PSRepository -Name 'PSGallery' -InstallationPolicy Trusted
+# Install-Script -Name winget-install -Force
+# winget-install.ps1 -Force
+
+
+#WebClient
+$dc = New-Object net.webclient
+$dc.UseDefaultCredentials = $true
+$dc.Headers.Add("user-agent", "Inter Explorer")
+$dc.Headers.Add("X-FORMS_BASED_AUTH_ACCEPTED", "f")
+
+#temp folder
+$InstallerFolder = $(Join-Path $env:ProgramData CustomScripts)
+if (!(Test-Path $InstallerFolder))
+{
+New-Item -Path $InstallerFolder -ItemType Directory -Force -Confirm:$false
+}
+	#Check Winget Install
+	Write-Host "Checking if Winget is installed" -ForegroundColor Yellow
+	$TestWinget = Get-AppxProvisionedPackage -Online | Where-Object {$_.DisplayName -eq "Microsoft.DesktopAppInstaller"}
+	If ([Version]$TestWinGet. Version -gt "2022.506.16.0") 
+	{
+		Write-Host "WinGet is Installed" -ForegroundColor Green
+	}Else 
+		{
+		#Download WinGet MSIXBundle
+		Write-Host "Not installed. Downloading WinGet..." 
+		$WinGetURL = "https://aka.ms/getwinget"
+		$dc.DownloadFile($WinGetURL, "$InstallerFolder\Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle")
+		
+		#Install WinGet MSIXBundle 
+		Try 	{
+			Write-Host "Installing MSIXBundle for App Installer..." 
+			Add-AppxProvisionedPackage -Online -PackagePath "$InstallerFolder\Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle" -SkipLicense 
+			Write-Host "Installed MSIXBundle for App Installer" -ForegroundColor Green
+			}
+		Catch {
+			Write-Host "Failed to install MSIXBundle for App Installer..." -ForegroundColor Red
+			} 
+	
+		#Remove WinGet MSIXBundle 
+		#Remove-Item -Path "$InstallerFolder\Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle" -Force -ErrorAction Continue
+		}
+
 
 # Function to install a package using winget
 function Install-WingetApp {
